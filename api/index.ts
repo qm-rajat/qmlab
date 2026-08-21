941450d5087c3156c08a87d45536e6bedc81ec25
Rajat Kumar Dash <119514152+qm-rajat@users.noreply.github.com>
2026-08-21 07:56:55 +0000

    refactor: simplify index.ts by removing unnecessary imports and setup

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,3 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import apiRoutes from "../server/routes/index.ts";
-
-dotenv.config();
-
-const app = express();
-
-app.use(express.json());
-app.use(cookieParser());
-app.use("/api", apiRoutes);
+import app from "../server.ts";
 
 export default app;
